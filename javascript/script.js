1b04c651c2ccffdbb4d412992047b67983f854f1
Gleb Belyaev <n5g@ya.ru>
2015-08-29 00:45:47 +0300

    javascript battleship verion 0.1

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,45 @@
-var word = "bootles";
-var count = 99;
-while (count > 0) {
-    console.log(count + " " + word + " of beer on the wall");
-    console.log(count + " " + word + " of beer,");
-    console.log("Take one down, pass it around,");
-    count = count - 1;
-    if (count > 0) {
-        console.log(count + " " + word + " of beer on the wall.");
+var randomLoc = Math.floor(Math.random() * 5);
 
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSink = false;
+
+while (isSink == false) {
+    guess = prompt("Готовся, целься, зажигай! (введите число 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("ВВедите коректные данные!");
     } else {
-        console.log("No more " + word + " of beer on the wall.");
+        guesses = guesses + 1;
+        var isHits = false;
+        if (location1 == guess) {
+            isHits = true;
+            location1 = -1;
+        } else if (location2 == guess) {
+            isHits = true;
+            location2 = -1;
+        } else
+        if (location3 == guess) {
+            isHits = true;
+            location3 = -1;
+        } else {
+            alert("промох");
+        }
+        if (isHits) {
+            alert("попал");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSink = true;
+                alert("Ты потопил мой кораболь!");
+            }
+        }
     }
 }
+
+
+var stats = "Ты сделал " + guesses + " выстрелов что бы потопить караболь, " + "что озночает что твоя точность " + (3 / guesses);
+alert(stats);
