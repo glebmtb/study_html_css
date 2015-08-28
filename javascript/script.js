49dbfb0a5efd35a5c9cf20fe5eed5a6b6f5c036c
Gleb Belyaev <n5g@ya.ru>
2015-08-28 17:22:38 +0300

    javascript change code

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,14 @@
-var scoops = 5;
-while (scoops > 0) {
-    document.write("Another scoop!<br>");
-    scoops = scoops - 1;
-    console.log(scoops);
+var word = "bootles";
+var count = 99;
+while (count > 0) {
+    console.log(count + " " + word + " of beer on the wall");
+    console.log(count + " " + word + " of beer,");
+    console.log("Take one down, pass it around,");
+    count = count - 1;
+    if (count > 0) {
+        console.log(count + " " + word + " of beer on the wall.");
+
+    } else {
+        console.log("No more " + word + " of beer on the wall.");
+    }
 }
-document.write("Life without ice cream isn't the same");
