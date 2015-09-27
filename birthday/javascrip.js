211136ecabc19b23134f892bf5538ba42e81e22e
Gleb Belyaev <n5g@ya.ru>
2015-09-27 17:50:53 +0300

    birthday add table

---
+++ birthday/javascrip.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Gleb on 27.09.15.
+ */
+
+
+
+$(document).ready(function () {
+    $('#list_birthday tbody').append('<tr><td>1</td><td>Лариса</td><td>23 мая 1986г.</td><td>День рождения</td><td>300</td></tr>');
+    $('#list_birthday tbody').append('<tr><td>2</td><td>Лариса</td><td>23 мая 1986г.</td><td>День рождения</td><td>300</td></tr>');
+    $('#list_birthday tbody').append('<tr><td>3</td><td>Лариса</td><td>23 мая 1986г.</td><td>День рождения</td><td>300</td></tr>');
+})
