30317ef14dbc3b913b4536bb4b3b21c5c9871ceb
Gleb Belyaev <n5g@ya.ru>
2015-09-29 21:28:59 +0300

    birtday

--- birthday/javascrip.js
+++ birthday/javascrip.js
@@ -5,7 +5,35 @@
 
 
 $(document).ready(function () {
-    $('#list_birthday tbody').append('<tr><td>1</td><td>Лариса</td><td>23 мая 1986г.</td><td>День рождения</td><td>300</td></tr>');
-    $('#list_birthday tbody').append('<tr><td>2</td><td>Лариса</td><td>23 мая 1986г.</td><td>День рождения</td><td>300</td></tr>');
-    $('#list_birthday tbody').append('<tr><td>3</td><td>Лариса</td><td>23 мая 1986г.</td><td>День рождения</td><td>300</td></tr>');
+    $('#list_birthday tbody').append('<tr><td>1</td><td>Лариса</td><td>23 мая 1986г.</td><td>300</td></tr>');
+    $('#list_birthday tbody').append('<tr class="alert"><td>2</td><td>Лариса</td><td>23 мая 1986г.</td><td>300</td></tr>');
+    $('#list_birthday tbody').append('<tr><td>3</td><td>Лариса</td><td>23 мая 1986г.</td><td>300</td></tr>');
 })
+
+var item = {
+    id: 0,
+    date: "",
+    name: "",
+    leftDay: 0
+
+}
+
+var model = {
+    addNewItem: function (item) {
+
+    },
+
+    loadList: function () {
+
+    },
+    deleteItem: function (id) {
+
+    },
+    changeItem: function(item){
+
+    }
+}
+
+var view = {}
+
+var control = {}
