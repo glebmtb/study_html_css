fc46c463ad7a984057979c7d0b16ab6e31e1a770
Gleb Belyaev <n5g@ya.ru>
2015-09-30 20:32:50 +0300

    birday

--- birthday/javascrip.js
+++ birthday/javascrip.js
@@ -8,6 +8,18 @@ $(document).ready(function () {
     $('#list_birthday tbody').append('<tr><td>1</td><td>Лариса</td><td>23 мая 1986г.</td><td>300</td></tr>');
     $('#list_birthday tbody').append('<tr class="alert"><td>2</td><td>Лариса</td><td>23 мая 1986г.</td><td>300</td></tr>');
     $('#list_birthday tbody').append('<tr><td>3</td><td>Лариса</td><td>23 мая 1986г.</td><td>300</td></tr>');
+
+    $("tr").click(function () {
+        if ($(this).is('[class*="active"]')) {
+            $(this).removeClass("active");
+            $("#btnChange").addClass("disabled");
+            $("#btnRemove").addClass("disabled");
+        } else {
+            $(this).addClass("active").siblings().removeClass("active");
+            $("#btnChange").removeClass("disabled");
+            $("#btnRemove").removeClass("disabled");
+        }
+    });
 })
 
 var item = {
