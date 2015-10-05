68885bcf45283b6fd0cd5a7092f1d97971653d34
Gleb Belyaev <n5g@ya.ru>
2015-10-05 20:48:16 +0300

    birthday add modal window

--- birthday/javascrip.js
+++ birthday/javascrip.js
@@ -44,4 +44,18 @@ $(document).ready(function () {
             $("#btnRemove").removeClass("disabled");
         }
     });
+
+    $('#formChangeItem').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var recipient = button.data('whatever'); // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        if('add'===recipient){
+            modal.find('.modal-title').text('Добавление нового друга');
+        }else if('cng'===recipient){
+            modal.find('.modal-title').text('Изминение данных друга');
+        }
+
+    })
 })
