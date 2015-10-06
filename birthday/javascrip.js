92b0d7f37f2f65f3883a67be847ad7ea499fecb9
Gleb Belyaev <n5g@ya.ru>
2015-10-06 16:20:48 +0300

    birthday load data in modal form

--- birthday/javascrip.js
+++ birthday/javascrip.js
@@ -4,33 +4,48 @@
 
 //var items = [item1, item2, item3];
 
-var itemsJSON = '[{"id": 1, "date": "23 мая 1986г.", "name": "Лариса", "leftDay": 300},{"id": 2, "date": "23 мая 1986г.", "name": "Петя", "leftDay": 300},{"id": 3, "date": "23 мая 1986г.", "name": "Сережа", "leftDay": 300}]';
-var items = JSON.parse(itemsJSON);
 
 var model = {
+    itemsJSON: '[{"id": 1, "date": "1986-05-23", "name": "Лариса", "leftDay": 300},{"id": 2, "date": "1986-05-23", "name": "Петя", "leftDay": 300},{"id": 3, "date": "1986-05-23", "name": "Сережа", "leftDay": 300}]',
+
     addNewItem: function (item) {
 
     },
 
     loadList: function () {
-
+        this.items = JSON.parse(this.itemsJSON);
+        return this.items;
     },
+
     deleteItem: function (id) {
 
     },
-    changeItem: function (item) {
 
+    updateItem: function (item) {
+
+    },
+
+    getById: function (id) {
+        for (var it in this.items) {
+            if(id == this.items[it].id){
+                return this.items[it];
+            }
+        }
     }
 }
 
-var view = {}
+var view = {
+    getSelectId: function() {
+        return $("#list_birthday tbody tr[class=active]").attr("id");
+    }
+}
 
 var control = {}
 
 $(document).ready(function () {
+    var items = model.loadList();
     for (var it in items) {
-
-        $('#list_birthday tbody').append('<tr><td>' + items[it].id + '</td><td>' + items[it].name + '</td><td>' + items[it].date + '</td><td>' + items[it].leftDay + '</td></tr>');
+        $('#list_birthday tbody').append('<tr id="' + items[it].id + '"><td>' + (1 + Number(it)) + '</td><td>' + items[it].name + '</td><td>' + items[it].date + '</td><td>' + items[it].leftDay + '</td></tr>');
     }
 
     $("tr").click(function () {
@@ -48,14 +63,18 @@ $(document).ready(function () {
     $('#formChangeItem').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget); // Button that triggered the modal
         var recipient = button.data('whatever'); // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this);
-        if('add'===recipient){
-            modal.find('.modal-title').text('Добавление нового друга');
-        }else if('cng'===recipient){
-            modal.find('.modal-title').text('Изминение данных друга');
-        }
 
+        var modal = $(this);
+        if ('add' === recipient) {
+            modal.find('.modal-title').text('Добавление нового друга');
+            modal.find('#inputName').val("");
+            modal.find('#inputDate').val("");
+        } else if ('cng' === recipient) {
+            modal.find('.modal-title').text('Изминение данных друга');
+            var id = view.getSelectId();
+            var item = model.getById(id);
+            modal.find('#inputName').val(item.name);
+            modal.find('#inputDate').val(item.date);
+        }
     })
 })
